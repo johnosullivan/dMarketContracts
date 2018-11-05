eb1c43fbd868f50b5219831cd77d98ac54238820
John O'Sullivan <josullivan1@luc.edu>
2018-11-04 22:13:16 -0600

    Test Deploying

--- truffle.js
+++ truffle.js
@@ -17,7 +17,7 @@ module.exports = {
     ganache: {
       host: "127.0.0.1",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "5777" // Match any network id
     }
   }
 };
